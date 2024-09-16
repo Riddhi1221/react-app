9808b2de82c606d1cf6356ee4ff5d0ca5bd523ec
Riddhi1221 <riddhichheta1221@gmail.com>
2024-09-16 09:00:49 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,29 @@ import Result from './Components/Result'
 function App() {
   return (
     <table border="1" cellSpacing="5" cellPadding="10">
-      <tr>
-        <th>NO</th>
-        <th>Name</th>
-        <th>sub1</th>
-        <th>sub2</th>
-        <th>sub3</th>
-        <th>sub4</th>
-        <th>sub5</th>
-        <th>Total</th>
-        <th>avg</th>
-        <th>min</th>
-        <th>max</th>
-      </tr>
-      <Result no="1" name="reena" Sub1="40" Sub2="60" Sub3="80" Sub4="90" Sub5="70"  />
-      <Result no="2" name="feni" Sub1="40" Sub2="60" Sub3="80" Sub4="90" Sub5="70" />
-      <Result no="3" name="harmi" Sub1="40" Sub2="60" Sub3="80" Sub4="90" Sub5="70" />
-      <Result no="4" name="janvi" Sub1="40" Sub2="60" Sub3="80" Sub4="90" Sub5="70" />
-      <Result no="5" name="chandrika" Sub1="40" Sub2="60" Sub3="80" Sub4="90" Sub5="70" />
-      <Result no="6" name="riddhi" Sub1="40" Sub2="60" Sub3="80" Sub4="90" Sub5="70" />
-
+        <tr>
+          <th>NO</th>
+          <th>Name</th>
+          <th>sub1</th>
+          <th>sub2</th>
+          <th>sub3</th>
+          <th>sub4</th>
+          <th>sub5</th>
+          <th>Total</th>
+          <th>avg</th>
+          <th>min</th>
+          <th>max</th>
+        </tr>
+        <Result no="1" name="reena" sub1="40" sub2="60" sub3="80" sub4="90" sub5="70" />
+        <Result no="2" name="feni" sub1="40" sub2="60" sub3="80" sub4="90" sub5="70" />
+        <Result no="3" name="harmi" sub1="40" sub2="60" sub3="80" sub4="90" sub5="70" />
+        <Result no="4" name="janvi" sub1="40" sub2="60" sub3="80" sub4="90" sub5="70" />
+        <Result no="5" name="chandrika" sub1="40" sub2="60" sub3="80" sub4="90" sub5="70" />
+        <Result no="6" name="riddhi" sub1="40" sub2="60" sub3="80" sub4="90" sub5="70" />
     </table>
   );
-};
+}
 
 export default App;
+
 
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -7,19 +7,20 @@ const Result = (props) => {
   let d = parseInt(props.sub4)
   let e = parseInt(props.sub5)
 
-  let total = a+b+c+d+e
-  let avg = total/5
-  let Max = Math.max(a,b,c,d,e)
-  let Min = Math.min(a,b,c,d,e)
+  let total = a + b + c + d + e
+  let avg = total / 5
+  let Max = Math.max(a, b, c, d, e)
+  let Min = Math.min(a, b, c, d, e)
+
   return (
     <tr>
       <td>{props.no}</td>
       <td>{props.name}</td>
-      <td>{props.Sub1}</td>
-      <td>{props.Sub2}</td>
-      <td>{props.Sub3}</td>
-      <td>{props.Sub4}</td>
-      <td>{props.Sub5}</td>
+      <td>{props.sub1}</td>
+      <td>{props.sub2}</td>
+      <td>{props.sub3}</td>
+      <td>{props.sub4}</td>
+      <td>{props.sub5}</td>
       <td>{total}</td>
       <td>{avg}</td>
       <td>{Min}</td>
