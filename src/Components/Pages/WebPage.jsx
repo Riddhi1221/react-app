e1e9a13f5b4c899651499e67d2f9ddb3e32ad91b
Riddhi1221 <riddhichheta1221@gmail.com>
2024-09-30 10:38:05 +0530

    add

--- src/Components/Pages/WebPage.jsx
+++ src/Components/Pages/WebPage.jsx
@@ -11,11 +11,16 @@ import Container from '@mui/material/Container';
 import InputBase from '@mui/material/InputBase';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardActionArea from '@mui/material/CardActionArea';
+import CastIcon from '@mui/icons-material/Cast';
 
-const pages = ['Home', 'Service', 'Portfolio','Team','Testimonial','Blog','Contact'];
+
+const pages = ['Home', 'Service', 'Portfolio', 'Team', 'Testimonial', 'Blog', 'Contact'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-// Styled components
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
@@ -45,7 +50,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`, // Corrected calc() interpolation
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('sm')]: {
@@ -78,93 +83,152 @@ function WebPage() {
   };
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-         <img src="	https://trydo.rainbowit.net/assets/images/logo/logo-light.png" alt="" />
+    <>
+      <AppBar position = "relative" className="hero" sx={{ backgroundImage: 'url(https://trydo.rainbowit.net/assets/images/bg/bg-image-26.jpg)' }}>
+        <Container maxWidth="xl">
+          <Toolbar  disableGutters>
+            <img src="https://trydo.rainbowit.net/assets/images/logo/logo-light.png" alt="Logo" />
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="open menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' },justifyContent: 'end',margin:'10px' }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="open menu"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
               >
-                {page}
-              </Button>
-            ))}
-          </Box>
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{ display: { xs: 'block', md: 'none' } }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
 
-          <Box sx={{zIndex: '999', color: '#fff' }}>
-          <Search>
-              <SearchIconWrapper>
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Buy Now"
-                inputProps={{ 'aria-label': 'search'}}
-              />
-            </Search>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'end', margin: '10px' }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
               ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            </Box>
+
+            <Box sx={{ zIndex: '999', color: '#fff' }}>
+              <Search sx={{ border: '1px solid' }}>
+                <SearchIconWrapper />
+                <StyledInputBase
+                  placeholder="Buy Now"
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </Search>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+          <div className="header">
+            <h1>Creative</h1>
+            <p>There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration.</p>
+            <button>Contact Us</button>
+          </div>
+        </Container>
+      </AppBar>
+
+    <Container sx={{display :'flex'}}>
+    <Card sx={{ maxWidth: 345, margin :'70px 0px 30px 0px' }}>
+        <CardActionArea>
+          {/* <CardMedia> */}
+            <CastIcon sx={{margin :' 10px 17px 0px 17px', fontSize :'45px',color :'red'}}/>
+          {/* </CardMedia> */}
+          
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" sx={{fontSize :'22px'}}>
+            Business Stratagy
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary',fontSize :'18px',color: 'rgb(138 135 135 / 95%)' }}>
+            I throw myself down among the tall grass by the stream as I lie close to the earth.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Card sx={{ maxWidth: 345, margin :'70px 0px 30px 0px' }}>
+        <CardActionArea>
+          {/* <CardMedia> */}
+            <CastIcon sx={{margin :' 10px 17px 0px 17px', fontSize :'45px',color :'red'}}/>
+          {/* </CardMedia> */}
+          
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" sx={{fontSize :'22px'}}>
+            Business Stratagy
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary',fontSize :'18px',color: 'rgb(138 135 135 / 95%)' }}>
+            I throw myself down among the tall grass by the stream as I lie close to the earth.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Card sx={{ maxWidth: 345, margin :'70px 0px 30px 0px' }}>
+        <CardActionArea>
+          {/* <CardMedia> */}
+            <CastIcon sx={{margin :' 10px 17px 0px 17px', fontSize :'45px',color :'red'}}/>
+          {/* </CardMedia> */}
+          
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" sx={{fontSize :'22px'}}>
+            Business Stratagy
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary',fontSize :'18px',color: 'rgb(138 135 135 / 95%)' }}>
+            I throw myself down among the tall grass by the stream as I lie close to the earth.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Container>
+
+
+      
+    </>
   );
 }
 
